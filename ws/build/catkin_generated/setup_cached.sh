ce73003be0d3e992bde2e40175e0bfcf52f815a0
Crask21 <crask21@student.sdu.dk>
2024-09-28 16:36:04 +0200

    Initial push

---
+++ ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/casper/Documents/studentermedhjælper/shadowbot/ws/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/casper/Documents/studentermedhjælper/shadowbot/ws/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/casper/Documents/studentermedhjælper/shadowbot/ws/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/casper/Documents/studentermedhjælper/shadowbot/ws/src:$ROS_PACKAGE_PATH"
